94a78dc9f9a7cd28e179f551f5b6419e55ea9a13
arminabdijs <arminabdijs@gmail.com>
2024-11-20 22:34:06 +0330

    Add files via upload

---
+++ 5-Variable/index.py
@@ -0,0 +1,33 @@
+variable = 5
+
+# ----------------------------------------------
+x, y = 500, 400
+
+# ----------------------------------------------
+num = 100
+print(num-1)
+
+# ----------------------------------------------
+x = 5
+x = "Armin"
+
+# ----------------------------------------------
+# How to properly define a variable
+myvar = "john"
+
+my_var = "john"
+
+_my_var = "john"
+
+myVar = "john"
+
+myVar2 = "john"
+
+# ----------------------------------------------
+# The wrong way to define a variable
+
+# 2myvar = "john"
+
+# my-var = "john"
+
+# my var = "john"
---
+++ 7-comments/index.py
@@ -0,0 +1,9 @@
+# this is comment
+
+print("Hello world!")  # this is comment
+
+"""
+this is comment
+written in 
+move than just one line
+"""
